572f784d9b12e84f6d101fc93067b4a7abfa22a6
Deep Bhupatkar <bhupatkardeep@gmail.com>
2025-02-11 17:33:56 +0530

    updated the xcframework

--- Package.swift
+++ Package.swift
@@ -1,5 +1,8 @@
 // swift-tools-version: 5.7
 // The swift-tools-version declares the minimum version of Swift required to build this package.
+// swift-tools-version: 5.7
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
 import PackageDescription
 
 let package = Package(
@@ -10,25 +13,16 @@ let package = Package(
     products: [
         .library(
             name: "VideoSDKRTCSwift",
-            targets: ["VideoSDKRTCSwift", "VideoSDKRTC", "WebRTC", "Mediasoup"]
-        ),
+            targets: ["VideoSDKRTCSwift", "VideoSDKRTC", "WebRTC", "Mediasoup", "SocketIO", "Starscream"]),
     ],
-    dependencies: [
-        .package(url: "https://github.com/socketio/socket.io-client-swift.git", from: "16.1.1") 
-    ],
+   dependencies: [],
     targets: [
-        .target(
-            name: "VideoSDKRTCSwift",
-            dependencies: [
-                .product(name: "SocketIO", package: "socket.io-client-swift")
-            ]
-        ),
+        .target(name: "VideoSDKRTCSwift", dependencies: []),
         .binaryTarget(name: "WebRTC", path: "Sources/WebRTC.xcframework"),
         .binaryTarget(name: "Mediasoup", path: "Sources/Mediasoup.xcframework"),
         .binaryTarget(name: "VideoSDKRTC", path: "Sources/VideoSDKRTC.xcframework"),
-        .testTarget(
-            name: "VideoSDKRTCSwiftTests",
-            dependencies: ["VideoSDKRTCSwift"]
-        ),
+        .binaryTarget(name: "SocketIO", path: "Sources/SocketIO.xcframework"),
+        .binaryTarget(name: "Starscream", path: "Sources/Starscream.xcframework"),
+        .testTarget(name: "VideoSDKRTCSwiftTests", dependencies: ["VideoSDKRTCSwift"]),
     ]
 )
