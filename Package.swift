beca9296cb07e6aad9133759ab449aa9e3a5fd4e
Deep Bhupatkar <bhupatkardeep@gmail.com>
2025-02-11 12:40:21 +0530

    remove starscream

--- Package.swift
+++ Package.swift
@@ -10,21 +10,20 @@ let package = Package(
     products: [
         .library(
             name: "VideoSDKRTCSwift",
-            targets: ["VideoSDKRTCSwift", "VideoSDKRTC", "WebRTC", "Mediasoup", "SocketIO"]
+            targets: ["VideoSDKRTCSwift", "VideoSDKRTC", "WebRTC", "Mediasoup"]
         ),
     ],
     dependencies: [
-        .package(url: "https://github.com/daltoniam/Starscream.git", from: "4.0.0")
+        .package(url: "https://github.com/socketio/socket.io-client-swift.git", from: "16.0.0") 
     ],
     targets: [
         .target(
             name: "VideoSDKRTCSwift",
-            dependencies: ["Starscream"]
+            dependencies: ["SocketIO"] 
         ),
         .binaryTarget(name: "WebRTC", path: "Sources/WebRTC.xcframework"),
         .binaryTarget(name: "Mediasoup", path: "Sources/Mediasoup.xcframework"),
         .binaryTarget(name: "VideoSDKRTC", path: "Sources/VideoSDKRTC.xcframework"),
-        .binaryTarget(name: "SocketIO", path: "Sources/SocketIO.xcframework"),
         .testTarget(
             name: "VideoSDKRTCSwiftTests",
             dependencies: ["VideoSDKRTCSwift"]
